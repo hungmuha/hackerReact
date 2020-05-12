a7d836f4ca44b4d945bdac329a4630db77221d22
hungmuha <hung.soboss@gmail.com>
2020-05-12 14:59:10 -0600

    abstract out the props properties to show on child component

--- src/App.js
+++ src/App.js
@@ -2,25 +2,74 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
+  const [searchTerm, setSearchTerm] = React.useState('React');
+  const handleSearch = event => {
+    setSearchTerm(event.target.value);
+  }
+  const searchedStories = stories.filter((story) => {
+      return story.title
+        .toLocaleLowerCase()
+        .includes(searchTerm.toLowerCase());
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My hacker stories</h1>
+      <Search search={searchTerm} onSearch={handleSearch}/>
+      <hr/>
+      <List list={searchedStories}/>
     </div>
   );
+};
+
+const Search = ({search,onSearch} )=> {
+  return(
+    <div>
+      <label htmlFor="search">Search:</label>
+      <input 
+        type="text" 
+        id= "search" 
+        onChange={onSearch}
+        value={search}  
+      />
+    </div>
+  )
+};
+
+const List = props => {
+  let display = props.list.map(item => {
+    return(
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>s
+        <span>{item.points}</span>
+      </div>
+    )
+  })
+  return(display);
 }
 
 export default App;
