66e2837e0d44024e335141c249da9ec00e74820d
hungmuha <hung.soboss@gmail.com>
2020-05-12 17:40:14 -0600

    make the original Item stateful and added inline function to remove the item

--- src/App.js
+++ src/App.js
@@ -2,43 +2,53 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+//custom hook to be reused
+const useSemiPersistentState = (key,initialState) => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key)||initialState);
+
+    //useEffect Hook to trigger side effect to localstorage everytime the seach term changed, it helps opt into the react components life cycle
+    //so we can show the latest searched term which is stored in the localstorage of browser API
+  React.useEffect(() => {
+    localStorage.setItem(key,value);
+  }, [value,key]);
+  
+  return [value, setValue];
+}
+
 const App = () => {
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org/',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org/',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-  ];
 
-  //custom hook to be reused
-  const useSemiPersistentState = (key,initialState) => {
-    const [value, setValue] = React.useState(
-      localStorage.getItem(key)||initialState);
-
-      //useEffect Hook to trigger side effect to localstorage everytime the seach term changed, it helps opt into the react components life cycle
-      //so we can show the latest searched term which is stored in the localstorage of browser API
-    React.useEffect(() => {
-      localStorage.setItem(key,value);
-    }, [value,key]);
-    
-    return [value, setValue];
-  }
   
   //passsing the key to overvome overwrting allocated item in local storage,
   //provide initialState key to prevent stale key
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search','React');
+  const [stories,setStories] = React.useState(initialStories);
+
+  const handleRemoveStory = item => {
+    const newStories = stories.filter(
+      story => item.objectID !== story.objectID
+    );
+
+    setStories(newStories);
+  };
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
@@ -64,13 +74,13 @@ const App = () => {
       </InputWithLabel>
 
       <hr/>
-      <List list={searchedStories}/>
+      <List list={searchedStories} onRemoveItem={handleRemoveStory}/>
     </div>
   );
 };
 
 const InputWithLabel = ({id,children,value,isFocused,type = 'text',onInputChange}) => {
-  
+
   const inputRef = React.useRef();
   React.useEffect(() => {
     if(isFocused && inputRef.current) {
@@ -93,18 +103,31 @@ const InputWithLabel = ({id,children,value,isFocused,type = 'text',onInputChange
   );
 }
 
-const List = ({ list }) =>
-  list.map(item => <Item key={item.objectID} item={item} />);
+const List = ({ list , onRemoveItem}) =>
+  list.map(item => (
+    <Item 
+      key={item.objectID}
+      item={item} 
+      onRemoveItem={onRemoveItem}
+    />
+  ));
 
-const Item = ({ item }) => (
-  <div>
-    <span>
-      <a href={item.url}>{item.title}</a>
-    </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-  </div>
-);
+const Item = ({ item, onRemoveItem }) => {
+  return(
+    <div>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      <span>
+        <button type="button" onClick= {() => onRemoveItem(item)}>
+          Dismiss
+        </button>
+      </span>
+    </div>
+  );
+};
 
 export default App;
