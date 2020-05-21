6c717ec7adc183abcf0fe483ad93f71452994a5d
hungmuha <hung.soboss@gmail.com>
2020-05-20 18:27:37 -0600

    changing functionality to making call only when the button is clicked

--- src/App.js
+++ src/App.js
@@ -61,11 +61,12 @@ const App = () => {
     { data:[],isLoading:false,isError:false }
   );
 
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
+
   const handleFetchStories = React.useCallback(() => {
-    if(!searchTerm) return;
 
     dispatchStories({ type: 'STORIES_FETCH_INIT'});
-    fetch(`${API_ENDPOINT}${searchTerm}`)
+    fetch(url)
       .then(response => response.json())
       .then(result => {
         dispatchStories({
@@ -76,7 +77,7 @@ const App = () => {
       .catch(() => 
         dispatchStories({type: 'STORIES_FETCH_FAILURE'})
       );
-  }, [searchTerm]);
+  }, [url]);
 
 React.useEffect(() => {
   handleFetchStories();
@@ -89,9 +90,13 @@ React.useEffect(() => {
     });
   };
 
-  const handleSearch = event => {
+  const handleSearchInput = event => {
     setSearchTerm(event.target.value);
   }
+
+  const handleSearchSubmit = () => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+  };
 
   const searchedStories = stories.data.filter((story) => {
       return story.title
@@ -108,10 +113,17 @@ React.useEffect(() => {
         label="Search" 
         value={searchTerm} 
         isFocused
-        onInputChange={handleSearch}> 
+        onInputChange={handleSearchInput}> 
         <strong>Search:</strong>
       </InputWithLabel>
 
+      <button
+        type="button"
+        disabled = {!searchTerm}
+        onClick={handleSearchSubmit}
+      >
+        Submit
+      </button>
       <hr/>
       {stories.isError && <p>Something went wrong ...</p>}
       {stories.isLoading ? (
